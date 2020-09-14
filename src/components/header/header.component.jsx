8f42b74e6b0ce9eba826d3cf2ec3af55f070e0fb
Mohammad Maninang <mohammadmaninang94@gmail.com>
2020-09-14 08:52:58 +0800

    created natours project

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './header.styles.scss';
+
+const Header = () => (
+    <header>
+        <div className='logo-box'>
+            <img src='/img/logo-white.png' alt='Natour Logo' className='logo' />
+        </div>
+        <div className='header-box'>
+            <h1 className='header-primary'>
+                <span className='heading-primary-main'>Outdoors</span>
+                <span className='heading-primary-sub'>is where life happens</span>
+            </h1>
+        </div>
+    </header>
+);
+
+export default Header;
