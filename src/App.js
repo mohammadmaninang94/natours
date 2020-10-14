96c0e07c2a5686dcc0744d1b1a839c4538b2dbdc
Mohammad Maninang <mohammadmaninang94@gmail.com>
2020-10-15 07:00:16 +0800

    added feature section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Fragment } from 'react';
 import Header from './components/header/header.component';
 import Main from './components/main/main.component';
+import FeaturesSection from './components/section-features/section-features.component';
 
 function App() {
   return (
     <Fragment>
       <Header />
       <Main />
+      <FeaturesSection />
     </Fragment>
   );
 }
