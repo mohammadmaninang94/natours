f3c46aeddb1b21851d6a04f268c26adb9b15760b
Mohammad Maninang <mohammadmaninang94@gmail.com>
2020-10-26 08:43:50 +0800

    finished nevigation part

--- src/components/navigation/navigation.component.jsx
+++ src/components/navigation/navigation.component.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 const Navigation = () => (
     <div className='navigation'>
-        <input type='checkbox' className='navigation__checkbox' id='navi-toggle'/>
-        <label htmlFor='navi-toggle' className='navigation__button'>Menu</label>
+        <input type='checkbox' className='navigation__checkbox' id='navi-toggle' />
+        <label htmlFor='navi-toggle' className='navigation__button'>
+            <span className='navigation__icon'></span>
+        </label>
         <div className='navigation__background'>&nbsp;</div>
         <nav className='navigation__nav'>
-            <ul className='navigation__list'> 
+            <ul className='navigation__list'>
                 <li className='navigation__item'><a href='#about' className='navigation__link'><span>01</span>About Natours</a></li>
                 <li className='navigation__item'><a href='#about' className='navigation__link'><span>02</span>Your benefits</a></li>
                 <li className='navigation__item'><a href='#about' className='navigation__link'><span>03</span>Popular tours</a></li>
