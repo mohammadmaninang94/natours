90f31ef26998c8265eeca86668d27ca475adff02
Mohammad Maninang <mohammadmaninang94@gmail.com>
2020-12-10 11:19:15 +0800

    Responsive images Density and Art switching with logo in footer component

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 const Footer = () => (
     <footer className='footer'>
         <div className='footer__logo-box'>
-            <img src='img/logo-green-2x.png' alt='Full Logo' className='footer__logo' />
+            <picture className='footer__logo'>
+                <source srcset='img/logo-green-small-1x.png 1x, img/logo-green-small-2x.png 2x' 
+                    media='(max-width: 37.5em)'/>
+                <img srcset='img/logo-green-1x.png 1x, img/logo-green-2x.png 2x' alt='Full Logo' />
+            </picture>
+
         </div>
         <div className='row'>
             <div className='col-1-of-2'>
@@ -20,7 +25,7 @@ const Footer = () => (
             <div className='col-1-of-2'>
                 <p className='footer__copyright'>
                     Built by <a href='#copyright' className='footer__link'>Mohammad Maninang</a>. Copyright &copy; by Mohammad Maninang. You are 100% allowed to use this webpage for both personal and commercial use but not to claim it as your own design.
-                    A credit to the original author, jonas Schmedtmann, is of course highly appreciated 
+                    A credit to the original author, jonas Schmedtmann, is of course highly appreciated
                 </p>
             </div>
         </div>
